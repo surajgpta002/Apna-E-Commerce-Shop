05ebb1d6d946a0e55a6031eb5f945a3751fe0a0c
Suraj Gupta <suraj20gupta02@gmail.com>
2023-12-12 01:13:09 +0530

    Bug fix

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
-const ApiFeatures = require("../utils/apiFeatures");
+const ApiFeatures = require("../utils/apifeatures");
 const cloudinary = require("cloudinary");
 
 //Create Product -- Admin
